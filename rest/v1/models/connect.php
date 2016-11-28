4b66ebaaebf15fb5bcefe37845766a4c32feef7c
garyDav <gary.2810.dav@gmail.com>
2016-11-28 16:59:07 -0400

    cambiando contraseña de la BD

--- rest/v1/models/connect.php
+++ rest/v1/models/connect.php
@@ -3,7 +3,7 @@
 function getConex() {
 	try {
 		$us = "root";
-		$pwd = "gary";
+		$pwd = "";
 		$conex = new PDO("mysql:host=localhost;dbname=pasajes;charset=utf8",$us,$pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 		$conex->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	}catch(PDOException $e) {
